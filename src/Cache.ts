7d182e11e43041098b981269cde52abb4dad4de2
Josef Jadrny <bc.josefjadrny@gmail.com>
2020-12-31 15:04:51 +0100

    feat: Function executeWithCache implemented

--- src/Cache.ts
+++ src/Cache.ts
@@ -1,8 +1,30 @@
-type FunctionToCache = (...args: any[]) => any
+type FunctionToExecute = (...args: any[]) => any
 
 export class Cache {
-    public async executeWithCache (func: FunctionToCache) {
-        const result = await func()
+    cache: Map<string, any>
+    tempCache: Map<string, FunctionToExecute>
+
+    constructor () {
+        this.cache = new Map()
+        this.tempCache = new Map()
+    }
+
+    public async executeWithCache (key: string, func: FunctionToExecute): Promise<any> {
+        if (typeof key !== 'string') throw new Error(`Key should be a string, got ${typeof key}`)
+
+        if (this.cache.has(key)) {
+            return this.cache.get(key)
+        }
+
+        if (this.tempCache.has(key)) {
+            return await this.tempCache.get(key)
+        }
+
+        this.tempCache.set(key, func())
+        const result = await this.tempCache.get(key)
+        this.cache.set(key, result)
+        this.tempCache.delete(key)
+
         return result
     }
 }
