d48341e649bf488acc45ae9aa74d65de96d62b97
Josef Jadrny <bc.josefjadrny@gmail.com>
2021-01-01 13:13:31 +0100

    test: Main tests for core functionalities

--- tests/Cache.test.ts
+++ tests/Cache.test.ts
@@ -1,16 +1,34 @@
 import { Cache } from '../src/Cache'
 let cache = new Cache()
 
-const resultOfAsyncFnc = 'ok'
-const asyncFnc = () => {
-    return new Promise(resolve => setTimeout(() => resolve(resultOfAsyncFnc), 100))
-}
+const resultOfAsyncMock = 'ok'
+const key = 'someKey'
+const asyncMock = jest.fn(() => {
+    return new Promise(resolve => setTimeout(() => resolve(resultOfAsyncMock), 50))
+})
 
 beforeEach(() => {
     cache = new Cache()
+    asyncMock.mockClear()
 })
 
 test('returns passed function if not cached', async () => {
-    const key = 'someKey'
-    expect(await cache.executeWithCache(key, asyncFnc)).toEqual(resultOfAsyncFnc)
+    expect(await cache.executeWithCache(key, asyncMock)).toEqual(resultOfAsyncMock)
+})
+
+test('returns cached function if is cached', async () => {
+    await cache.executeWithCache(key, asyncMock)
+    expect(await cache.executeWithCache(key, asyncMock)).toEqual(resultOfAsyncMock)
+    expect(asyncMock.mock.calls.length).toBe(1)
+})
+
+test('async function should be executed only once in async calls', async () => {
+    const promiseAll = Promise.all([
+        cache.executeWithCache(key, asyncMock),
+        cache.executeWithCache(key, asyncMock),
+        cache.executeWithCache(key, asyncMock),
+    ])
+    const result = await promiseAll
+    expect(asyncMock.mock.calls.length).toBe(1)
+    expect(result).toEqual([resultOfAsyncMock, resultOfAsyncMock, resultOfAsyncMock])
 })
