831338dc7f86f6eb45c37ed9896833ea42701d86
Josef Jadrny <bc.josefjadrny@gmail.com>
2021-01-02 14:34:50 +0100

    tests: Add more tests

--- tests/Cache.test.ts
+++ tests/Cache.test.ts
@@ -22,6 +22,14 @@ test('returns cached function if is cached', async () => {
     expect(asyncMock.mock.calls.length).toBe(1)
 })
 
+test('should properly save results to cache', async () => {
+    const functionToCache = async () => { return 'test' }
+    await cache.executeWithCache(key, functionToCache)
+    await cache.executeWithCache(key, asyncMock)
+    await cache.executeWithCache('another key', asyncMock)
+    expect(cache.cache).toEqual(new Map([[key, 'test'], ['another key', 'ok']]))
+})
+
 test('async function should be executed only once in async calls', async () => {
     const promiseAll = Promise.all([
         cache.executeWithCache(key, asyncMock),
@@ -31,4 +39,10 @@ test('async function should be executed only once in async calls', async () => {
     const result = await promiseAll
     expect(asyncMock.mock.calls.length).toBe(1)
     expect(result).toEqual([resultOfAsyncMock, resultOfAsyncMock, resultOfAsyncMock])
+})
+
+test('should throw an error when key is not a string', async () => {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    await expect(cache.executeWithCache({}, asyncMock)).rejects.toThrow('Key should be a string, got object')
 })
